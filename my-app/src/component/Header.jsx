f693c038381c4a42ed2177b289791ffc770ea3ca
sontv8 <sontv8@fpt.edu.vn>
2023-07-15 11:14:40 +0700

    day4 - setup react project + component

---
+++ my-app/src/component/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Product</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ my-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { Header, Footer } from './component'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Header />
+    <main></main>
+    <Footer />
+  </>
+)
