55afe10d4fc78533a97ba19b8ef1ddb2d31dfbae
sontv8 <sontv8@fpt.edu.vn>
2023-07-18 10:59:00 +0700

    day5 - update,delete product

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -6,6 +6,19 @@ const HomePage = ({ data }) => {
     const addProduct = (product) => {
         setProducts([...products, product])
     }
+
+    const deleteProduct = (id) => {
+        const newData = products.filter((product) => product.id != id)
+        // console.log("New data:", newData);
+        setProducts(newData)
+    }
+
+    const updateProduct = (id) => {
+        const newData = products.map((product) => product.id == id ? { id: 1, name: "product update", price: 1000 } : product)
+        // console.log("Old data:", products);
+        // console.log("New data: ", newData);
+        setProducts(newData)
+    }
     return (
         <div>
             <button onClick={() => addProduct({ id: 4, name: "Product 4", price: 400 })}>Add New Product</button>
@@ -14,6 +27,8 @@ const HomePage = ({ data }) => {
                     <div key={index + 1}>
                         <h3>{product.name}</h3>
                         <p>{product.price}</p>
+                        <button onClick={() => deleteProduct(product.id)}>Delete</button>
+                        <button onClick={() => updateProduct(product.id)}>Update</button>
                     </div>
                 )
             })}
